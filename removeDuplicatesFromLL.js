39a92bdb236d189b183e0e3e4121fcc19eff8436
IcarusGit <sebastianicarus52@gmail.com>
2023-11-19 03:31:35 +0800

    feat: finally answered a linked list problem in leetcode

---
+++ removeDuplicatesFromLL.js
@@ -0,0 +1,33 @@
+/*
+Example 1:
+Input: head = [1,1,2]
+Output: [1,2]
+
+Example 2:
+Input: head = [1,1,2,3,3]
+Output: [1,2,3]
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    let current = head;
+
+    while(current && current.next){
+        if(current.val === current.next.val){
+            current.next = current.next.next
+        } else {
+            current = current.next
+        }
+    } 
+    return head
+};
