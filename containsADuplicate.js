d5e28d35572e757c5b70f416bc3db46e21cb68d8
IcarusGit <sebastianicarus52@gmail.com>
2023-11-23 00:43:26 +0800

    feat: finally learned how to check if there is a duplicate but this is only returning booleans

---
+++ containsADuplicate.js
@@ -0,0 +1,32 @@
+/*
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false
+
+Example 3:
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    // nums.sort((a,b) => a - b)
+
+    // for (let i = 1; i < nums.length; i++){
+    //     if (nums[i] === nums[i-1]) return true
+    // }
+    // return false
+
+    return new Set(nums).size !== nums.length
+};
+
+let nums = [1,1,1,3,3,4,3,2,4,2]
+// let nums = [1,2,3,4]
+console.log(containsDuplicate(nums))
