067cee6c46875db1f81fd183a2507282c59c0ce0
IcarusGit <sebastianicarus52@gmail.com>
2023-12-15 23:09:24 +0800

    fix: added the comment and added another solution for better understanding

--- highestAltitude.js
+++ highestAltitude.js
@@ -14,17 +14,27 @@ Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.
  * @return {number}
  */
 var largestAltitude = function(gain) {
+    // gumawa muna ako nung paglalagyan ng maximum pati nung current peaks kada index
     let maxAltitude = 0
     let currentAltitude = 0
 
     for (let i = 0; i < gain.length; i++){
+        // so shempre mag sstart sa 0 diba so ang ginawa ko equals 0 na muna yung max at current
+        // so si current shempre iaadd ko lang sakanya si gain so possible maging negative or positive to basta add lang ng add 
+        // para malaman yung mga narating niyang peaks
         currentAltitude += gain[i]
+        
+        // si max shempre i mamath.math ko yung pinakamataas na peak na narating niya
+        // pag di mo nagets to check mo yung if statement na nakacomment para magets mo yung nangyayare
         maxAltitude = Math.max(maxAltitude, currentAltitude)
+        // if (currentAltitude > maxAltitude){
+        //     maxAltitude = currentAltitude
+        // }
     }
 
     return maxAltitude
 };
 
-let gain = [-4,-3,-2,-1,4,3,2]
+let gain = [-5,1,5,0,-7]
 
 console.log(largestAltitude(gain))
